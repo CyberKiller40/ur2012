b3d2319ffafe44798db461887cca6149e647fa34
Łukasz "Cyber Killer" Korpalski <cyberkiller8@gmail.com>
2012-09-12 06:07:57 +0200

    pierwsza wersja
    --HG--
    extra : convert_revision : cyberkiller8%40gmail.com-20120912040757-y3lszu98shkfgeht

---
+++ czyjuzczas.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2012-09-12T05:35:28
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+TARGET = czyjuzczas
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        widget.cpp
+
+HEADERS  += widget.h
+
+FORMS    += widget.ui
---
+++ widget.cpp
@@ -0,0 +1,34 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+#include <cstdlib>
+#include <QDateTime>
+
+Widget::Widget(QWidget *parent) :
+	QWidget(parent),
+	ui(new Ui::Widget)
+{
+	ui->setupUi(this);
+	srand(time(0));
+}
+
+Widget::~Widget()
+{
+	delete ui;
+}
+
+void Widget::on_pushButton_clicked()
+{
+	if((rand() % 20 + 1) == 1)
+	{
+		ui->lineEdit->setText("The cake is a lie!");
+	}
+	else if(QDateTime::currentDateTime()>QDateTime(QDate(2012,9,12),QTime(6,6)))
+	{
+		ui->lineEdit->setText("TAK! Ciasto jest w kuchni od 10:15! :-)");
+	}
+	else
+	{
+		ui->lineEdit->setText("Nie :-P.");
+	}
+}
